c364efe1169b0a50e2ed5b082df02e1fd53c6c25
hpal <hpal@visteon.com>
2024-12-26 20:58:16 +0530

    accuweather: send only status no checks

--- api/config.js
+++ api/config.js
@@ -1,25 +1,8 @@
 // /pages/api/config.js
 export default function handler(req, res) {
-    if (req.method !== 'GET') {
-        return res.status(405).json({
-            status: 'error',
-            message: 'Method not allowed'
-        });
-    }
-
-    // Verify client secret
-    const clientApiKey = req.headers['x-api-key'];
-    if (!clientApiKey || clientApiKey !== process.env.CLIENT_SECRET) {
-        return res.status(401).json({
-            status: 'error',
-            message: 'Unauthorized'
-        });
-    }
-
-    // Simple response for app
     res.status(200).json({
         data: {
-            apikey: process.env.ACCUWEATHER_API_KEY
+            apikey: process.env.ACCUWEATHER_API_KEY,
         }
     });
 }
